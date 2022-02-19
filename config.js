cf9c9ff69758f3ef8cf32c56908185d043128281
Joa00721 <86857876+Joa00721@users.noreply.github.com>
2022-02-19 15:23:09 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Pega tu configuración de firebase aquí
+const firebaseConfig = {
+    apiKey: "AIzaSyDRVd9grZuPVQkUo9n9BLSDjEuI9b60Has",
+    authDomain: "proyecto-71.firebaseapp.com",
+    projectId: "proyecto-71",
+    storageBucket: "proyecto-71.appspot.com",
+    messagingSenderId: "1084399647347",
+    appId: "1:1084399647347:web:0cf49e4face3af95c04806"
+  };
+  
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
